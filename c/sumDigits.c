f497a27b530678f3165776886c03f77385cf83a9
Nickson K Rop <nickyrutto@gmail.com>
2023-03-08 13:31:06 +0000

    sum all digits in a given integr

--- c/sumDigits.c
+++ c/sumDigits.c
@@ -6,16 +6,14 @@ unsigned int sumDigits(unsigned int n)
   if (n == 0)
   {
     return 0;
-  } else  
-  {
-    return n+sumDigits(n-1);
   }
+  return (n%10)+sumDigits(n/10);
 }
 
 void main()
 {
   int res;
-  int i = 5;
+  int i = 5913;
   res = sumDigits(i);
   printf("The sum is %d\n",res);
   
