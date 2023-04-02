29e834b88428a41b0290e861f2dc307aaa499afe
Nickson K Rop <nickyrutto@gmail.com>
2023-04-02 14:49:20 +0000

    practice question

---
+++ c/quiz1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("%d\n", printf("%s", "Hello World!\n"));
+  return (0);
+}
---
+++ c/quiz4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+  unsigned i = 1;
+  int j = -4;
+  printf("%u\n", i + j);
+  return (0);
+}
