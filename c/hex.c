e995e5989915d12ed010fff005a46c2d1508be78
Nickson K Rop <nickyrutto@gmail.com>
2023-04-17 11:25:57 +0000

    excercise file

---
+++ c/hex.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print hexadecimal value of a variable
+ * @var: value to be printed in hexadecimal
+ * Return: 0 on success
+ */
+
+int main()
+{
+	int var = 0x43FF;
+	printf("%x\n", var);
+	return 0;
+}
---
+++ c/static.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+static int i;
+static int i = 27;
+static int i;
+int main()
+{
+	int i;
+	printf("%d\n", i);
+	return (0);
+}
